93947a30164da218ba7758794e7d983e017d223e
mgrouch <mgrouch>
2002-11-23 20:39:16 +0000

    do I need libtool?

--- autogen.sh
+++ autogen.sh
@@ -17,24 +17,19 @@ DIE=0
 	DIE=1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile xmlstarlet."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/libtool/libtool-1.4.2.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
+#(libtool --version) < /dev/null > /dev/null 2>&1 || {
+#	echo
+#	echo "You must have libtool installed to compile xmlstarlet."
+#	echo "Get ftp://ftp.gnu.org/pub/gnu/libtool/libtool-1.4.2.tar.gz"
+#	echo "(or a newer version if it is available)"
+#	DIE=1
+#}
 
-(automake-1.4 --version) < /dev/null > /dev/null 2>&1 || {
+(automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have latest automake 1.4 installed to compile xmlstarlet,"
-        echo "or alternatively create a symlink from automake-1.4 to "
-        echo "plain automake."
-        echo "Newer versions of automake 1.4 come with the symlink "
-        echo "pregenerated. This will allow you to compile xmlstarlet "
-        echo "while also installing newer automakes such as 1.6."
+	echo "You must have automake 1.4 or newer installed to compile xmlstarlet"
 	echo "Get ftp://sources.redhat.com/pub/automake/automake-1.4l.tar.gz"
-	echo "(or a newer version in the 1.4 series if it is available)."              
+	echo "(or a newer version if it is available)."              
 	DIE=1
 }
 
@@ -52,9 +47,9 @@ if test -z "$*"; then
         echo "to pass any to it, please specify them on the $0 command line."
 fi
 
-libtoolize --copy --force
-aclocal-1.4 $ACLOCAL_FLAGS
-automake-1.4 --add-missing
+#libtoolize --copy --force
+aclocal $ACLOCAL_FLAGS
+automake --add-missing
 autoconf
 
 cd $THEDIR
