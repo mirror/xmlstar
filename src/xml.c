a591d52480b85c5676c236d1cd4c8d41ac020161
mgrouch <mgrouch>
2002-11-23 05:33:28 +0000

    work in progress

--- src/xml.c
+++ src/xml.c
@@ -1,29 +1,30 @@
-/* $Id: xml.c,v 1.10 2002/11/22 06:22:16 mgrouch Exp $ */
+/* $Id: xml.c,v 1.11 2002/11/23 05:33:28 mgrouch Exp $ */
 
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+static const char usage_str[] =
+"XMLStarlet Toolkit: Command line utilities for XML\n"
+"Usage: xml <command> [<options>]\n"
+"where <command> is one of:\n"
+"   ed   (or edit)      - Edit XML document(s)\n"
+"   sel  (or select)    - Select data or query XML document(s)\n"
+"   tr   (or transform) - Transform XML document(s)\n"
+"   val  (or validate)  - Validate XML document(s)\n"
+#if 0
+/* TODO */
+"   diff                - Find differences between XML documents\n"
+#endif
+
+"Type: xml <command> --help <ENTER> for command help\n\n"
+"XMLStarlet is a command line toolkit to query/edit/check/transform\n"
+"XML documents (for more information see http://xmlstar.sourceforge.net/)\n";
+
 void usage(int argc, char **argv)
 {
     FILE* o = stderr;
-
-    fprintf(o, "XMLStarlet Toolkit: Command line utilities for XML\n");
-    fprintf(o, "Usage: xml <command> [<options>]\n");
-    fprintf(o, "where <command> is one of:\n");
-    fprintf(o, "      ed   (or edit)      - Edit XML document\n");
-    fprintf(o, "      sel  (or select)    - Select data or query XML document(s)\n");
-    fprintf(o, "      tr   (or transform) - Transform XML document\n");
-    fprintf(o, "      val  (or validate)  - Validate XML document(s)\n");
-#if 0
-    /* TODO */
-    fprintf(o, "      diff                - Find differences between XML documents\n");
-#endif
-
-    fprintf(o, "Type: xml <command> --help <ENTER> for more options\n\n");
-    fprintf(o, "XMLStarlet is a command line toolkit to query/edit/check/transform\n");
-    fprintf(o, "XML documents (for more information see http://xmlstar.sourceforge.net/)\n");
-
+    fprintf(o, usage_str);
     exit(1);
 }  
 
@@ -31,23 +32,30 @@ int main(int argc, char **argv)
 {
     int ret = 0;
 
+    extern int xml_edit(int argc, char **argv);
+    extern int xml_select(int argc, char **argv);
+    extern int xml_trans(int argc, char **argv);
+    extern int xml_validate(int argc, char **argv);
+    
     if (argc <= 1) usage(argc, argv);
+
     if (!strcmp(argv[1], "ed") || !strcmp(argv[1], "edit"))
     {
         ret = xml_edit(argc, argv);
-    } else
-    if (!strcmp(argv[1], "sel") || !strcmp(argv[1], "select"))
+    }
+    else if (!strcmp(argv[1], "sel") || !strcmp(argv[1], "select"))
     {
         ret = xml_select(argc, argv);
-    } else
-    if (!strcmp(argv[1], "tr") || !strcmp(argv[1], "transform"))
+    }
+    else if (!strcmp(argv[1], "tr") || !strcmp(argv[1], "transform"))
     {
         ret = xml_trans(argc, argv);
-    } else
-    if (!strcmp(argv[1], "val") || !strcmp(argv[1], "validate"))
+    }
+    else if (!strcmp(argv[1], "val") || !strcmp(argv[1], "validate"))
     {
         ret = xml_validate(argc, argv);
-    } else
+    }
+    else
     {
         usage(argc, argv);
     }
