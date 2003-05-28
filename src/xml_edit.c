1036a381ba3a86b325d6653b39f018e3c1afb9ea
mgrouch <mgrouch>
2003-05-28 17:36:11 +0000

    some code cleanup

--- src/xml_edit.c
+++ src/xml_edit.c
@@ -1,4 +1,4 @@
-/*  $Id: xml_edit.c,v 1.21 2003/05/28 17:33:19 mgrouch Exp $  */
+/*  $Id: xml_edit.c,v 1.22 2003/05/28 17:36:11 mgrouch Exp $  */
 
 /*
 
@@ -292,53 +292,9 @@ edUpdate(xmlDocPtr doc, const char *loc, const char *val, XmlNodeType type)
             for (i = 0; i < cur->nodeNr; i++)
             {
                 /*
-                 *  rename node
+                 *  update node
                  */
-                /*
-                if (cur->nodeTab[i]->type == XML_ATTRIBUTE_NODE)
-                {
-                    xmlFree((xmlChar *) cur->nodeTab[i]->name);
-                    cur->nodeTab[i]->name = xmlStrdup((const xmlChar*) val);
-                }
-                else
-                {
-                    xmlFree((xmlChar *)cur->nodeTab[i]->name);
-                    cur->nodeTab[i]->name = xmlStrdup((const xmlChar*) val);
-                }
-                */
-                if (cur->nodeTab[i]->type == XML_ATTRIBUTE_NODE)
-                {
-                    //printf("%p, %s\n", xmlSetProp(cur->nodeTab[i]->parent, cur->nodeTab[i]->name, val), cur->nodeTab[i]->name);
-                    //xmlChar* tmp = xmlStrdup(cur->nodeTab[i]->name);
-                    //xmlNodePtr node = cur->nodeTab[i]->parent;
-                    xmlAttrPtr attr = NULL;
-                    attr = cur->nodeTab[i];
-                    xmlNodeSetContent(attr, val);
-                    //xmlFreeNode(attr->children);
-                    //if (attr->children != NULL) xmlFreeNodeList(attr->children);
-                    //attr->children = NULL;
-                    //UPDATE_LAST_CHILD_AND_PARENT(attr)
-                    //xmlUnsetProp(cur->nodeTab[i]->parent, cur->nodeTab[i]->name);
-                    //xmlSetProp(node, tmp, val);
-                    //xmlRemoveProp(cur->nodeTab[i]);
-                    //xmlFreeProp(cur->nodeTab[i]);
-                    //xmlSetProp(cur->nodeTab[i], cur->nodeTab[i]->name, val);
-                    //attr = xmlNewProp(node, tmp, xmlStrdup(val));
-                    //attr = xmlNewProp(node, "a", "v");
-                    //attr->name = tmp;
-                //printf("eeeeee %s node %s\n", tmp, node->name);
-                    //xmlNodeSetContent(((xmlAttrPtr) cur->nodeTab[i])->children, val);
-                    //xmlFree((xmlChar *)cur->nodeTab[i]->content);
-                    //((xmlAttrPtr) cur->nodeTab[i])->children->content = xmlStrdup((const xmlChar*) val);
-                    //cur->nodeTab[i]->content = xmlStrdup((const xmlChar*) val);
-                    //if (cur->nodeTab[i]->children != NULL) xmlFreeNodeList(cur->nodeTab[i]->children);
-                    //xmlFree((xmlChar *)((xmlAttributePtr) cur->nodeTab[i])->defaultValue);
-                    //((xmlAttributePtr) cur->nodeTab[i])->defaultValue = xmlStrdup((const xmlChar*) val);
-                }
-                else
-                {
-                    xmlNodeSetContent(cur->nodeTab[i], val);
-                }
+                xmlNodeSetContent(cur->nodeTab[i], BAD_CAST val);
             }
             break;
         }
