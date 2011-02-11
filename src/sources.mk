3b0061719a992225c9659f62887431bebc9a4721
Noam Postavsky <npostavs@users.sourceforge.net>
2011-02-10 23:10:43 -0500

    seperate list of sources and tests into subdirs

---
+++ src/sources.mk
@@ -0,0 +1,23 @@
+xml_SOURCES =\
+src/binsert.c\
+src/binsert.h\
+src/escape.h\
+src/stack.c\
+src/stack.h\
+src/strdup.c\
+src/strdup.h\
+src/trans.c\
+src/trans.h\
+src/xml.c\
+src/xml_C14N.c\
+src/xml_depyx.c\
+src/xml_edit.c\
+src/xml_elem.c\
+src/xml_escape.c\
+src/xml_format.c\
+src/xml_ls.c\
+src/xml_pyx.c\
+src/xml_select.c\
+src/xmlstar.h\
+src/xml_trans.c\
+src/xml_validate.c
