575972da87526c96e764db5c7c9fe3a2aca6fd39
Noam Postavsky <npostavs@users.sourceforge.net>
2012-06-17 19:17:54 -0400

    doc building is conditional

--- doc/doc.mk
+++ doc/doc.mk
@@ -1,5 +1,6 @@
-userguide = doc/xmlstarlet-ug.pdf doc/xmlstarlet-ug.ps doc/xmlstarlet-ug.html
-userguide_src = doc/xmlstarlet-ug.xml
+userguide = doc/xmlstarlet-ug
+userguide_gen = $(userguide).pdf $(userguide).ps $(userguide).html
+userguide_src = $(userguide).xml
 
 txtguide = doc/xmlstarlet.txt
 txtguide_src = doc/gen-doc
@@ -11,9 +12,11 @@ DOCBOOK_PARAMS = \
 --param section.autolabel 1 \
 --stringparam generate.toc 'book toc,title'
 
-EDIT_XML = xml ed -P \
+EDIT_XML = ./xml ed -P \
 -u '//db:phrase[@role="VERSION"]' -v '$(VERSION)' \
 -u '//db:phrase[@role="PROG"]' -v "`echo xml | sed '$(program_transform_name)'`"
+
+if BUILD_DOCS
 
 .xml.html:
 	$(EDIT_XML) $< | xsltproc $(DOCBOOK_PARAMS) \
@@ -24,15 +27,23 @@ EDIT_XML = xml ed -P \
 .xml.fo:
 	$(EDIT_XML) $< | xsltproc $(DOCBOOK_PARAMS) doc/xmlstar-fodoc-style.xsl - > $@
 
+if HAVE_FOP
 .fo.pdf:
-	fop -q $< $@
+	$(FOP) -q $< $@
+endif
+if HAVE_PDF2PS
 .pdf.ps:
-	pdf2ps $< $@
+	$(PDF2PS) $< $@
+endif
+
+$(userguide).html : $(userguide).xml | xml$(EXEEXT)
 
 $(manpage): $(manpage_src)
 	 $(EDIT_XML) $< | xsltproc -o $@ \
 	  http://docbook.sourceforge.net/release/xsl-ns/current/manpages/docbook.xsl \
 	  -
 
-$(txtguide): $(txtguide_src) xml
+$(txtguide): $(txtguide_src) | xml$(EXEEXT)
 	srcdir=$(srcdir) transform='$(program_transform_name)' $< ./xml > $@
+
+endif # BUILD_DOCS
