47958965ffa0b5e7444e41958beeeb6a9d3dfdd9
Noam Postavsky <npostavs@users.sourceforge.net>
2014-03-02 19:13:54 -0500

    Revert "give a helpful message if doc has default namespace and nothing matched"
    This reverts commit 5cb1977bb3127443415a9952b0afad5be9fb54b7.
    
    Conflicts:
    	src/xml_edit.c

--- src/xmlstar.h
+++ src/xmlstar.h
@@ -48,7 +48,6 @@ void registerXstarVariable(xmlXPathContextPtr ctxt,
     const char* name, xmlXPathObjectPtr value);
 void registerXstarNs(xmlXPathContextPtr ctxt);
 
-const char* DEFAULT_NS_FAIL_MESSAGE;
 extern const xmlChar *default_ns;
 
 int parseNSArr(xmlChar** ns_arr, int* plen, int argc, char **argv);
